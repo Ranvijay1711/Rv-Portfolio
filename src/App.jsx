010c3bbc4de8267cf9d03d0b18a9aacba7361006
Ranvijay1711 <ranvijay255217@gmail.com>
2025-08-08 05:22:47 +0530

    changes icon

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import './index.css'
+import { div } from 'framer-motion/client'
+
+const App=()=>{
+  return(
+    <div>
+      3D Developer Portfolio
+    </div>
+  )
+}
+export default App
